eca2e9603a83485fc48acee8e8915d297376f652
Nicholas Hedges <nshedges@aggiemail.usu.edu>
2019-11-21 09:58:40 -0700

    Add timestamp reports and md5 checksum at the end

--- 7zPushFiles.sh
+++ 7zPushFiles.sh
@@ -3,6 +3,11 @@ if ! (($# >= 2)) ; then
 	echo "Usage: 7zPushDir.sh [filename of 7z archive] [files to push]"
 	exit 0
 fi
+echo "Began "$(date)
 SCRIPT_DIR=$(dirname $(realpath -s $0))
 7z a -p -mhe $SCRIPT_DIR/Google_Drive/$1 "${@:2}"
+echo "7z compression completed "$(date)
 $HOME/go/bin/drive push -ignore-checksum=false -no-prompt $SCRIPT_DIR/Google_Drive/$1
+echo "Upload completed "$(date)
+md5sum $SCRIPT_DIR/Google_Drive/$1
+echo "Checksum completed "$(date)
