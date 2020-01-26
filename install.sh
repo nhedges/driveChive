46a32c245be81fc3a4d0c77ec888cbcd06fb0207
Nicholas Hedges <nshedges@aggiemail.usu.edu>
2020-01-26 15:19:10 -0700

    pushdir appears to be working, with md5sum, unknown changes to install.

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-chmod +x installGo.sh
+OS=linux
+ARCH=amd64
+VERSION=1.13.4
 if ! [ -d "/usr/local/go" ] ; then
-	sudo ./installGo.sh
+	echo "Downloading GO..."
+	sudo curl https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz --output go$VERSION.$OS-$ARCH.tar.gz
+	sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 fi
 
 if ! [ -f "$HOME/go/bin/drive" ] ; then
+	echo "Installing Drive Client, please wait..."
 	/usr/local/go/bin/go get -u github.com/odeke-em/drive/cmd/drive
 fi
 
@@ -12,4 +17,6 @@ if ! [ -d "Google_Drive" ] ; then
     mkdir Google_Drive
     chown -R `stat -c '%U' $HOME` Google_Drive
 fi
+echo "Initializing Google Drive directory. Ready to Log in."
 $HOME/go/bin/drive init ./Google_Drive
+echo "Google drive auto upload is ready. use other script(s) to automatically archive things onto Google Drive."
