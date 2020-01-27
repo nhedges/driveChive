1c0905c9dd7da49fd62007e3b560b06a894c928d
Nicholas Hedges <nshedges@aggiemail.usu.edu>
2020-01-26 22:14:00 -0700

    Add user information to the install script

--- install.sh
+++ install.sh
@@ -2,6 +2,25 @@
 OS=linux
 ARCH=amd64
 VERSION=1.13.4
+echo "Welcome to the driveChive installer. This script was made to set up convenient and fast archiving on linux. It has a few steps."
+echo ----------
+echo "1) If GO has not been installed in /usr/local, it will be downloaded from google and extracted/installed in /usr/local."
+echo ----------
+echo "2) If 'drive' is not found in the ~/go/bin/ folder, then the Google Drive client will be installed. (github.com/odeke-em/drive/cmd/drive)"
+echo ----------
+echo "3) If the Google_Drive folder is not found in the current directory, it will be made in the current working directory. This will contain the files uploaded to Google Drive. "
+echo ----------
+echo "4) Google drive will be initialized and you will need to sign in."
+echo ----------
+echo "5) After this the other commands will be ready to automate archiving, as long as the scripts are located in the same directory as Google_Drive folder."
+echo ----------
+read -p "Are you ready to continue? [Y/n]:" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo 
+  exit 1
+fi
+
 if ! [ -d "/usr/local/go" ] ; then
 	echo "Downloading GO..."
 	sudo curl https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz --output go$VERSION.$OS-$ARCH.tar.gz
