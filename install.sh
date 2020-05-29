22bd68d57ca201843446fe8bd18f3f7337a90179
Nicholas Hedges <nshedges@aggiemail.usu.edu>
2020-05-29 00:46:08 -0600

    Update install script for local folder/portable install

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ VERSION=1.13.4
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Welcome to the driveChive installer. This script was made to set up convenient and fast archiving on linux. It has a few steps."
 echo ----------
-echo "1) If GO has not been installed in /usr/local, it will be downloaded from google and extracted/installed in /usr/local."
+echo "1) If GO has not been installed in /usr/local, it will be downloaded from google and extracted/installed within this repository directory."
 echo ----------
-echo "2) If 'drive' is not found in the ~/go/bin/ folder, then the Google Drive client will be installed. (github.com/odeke-em/drive/cmd/drive)"
+echo "2) If 'drive' is not found in this folder/gocode/bin/, then the Google Drive client will be installed. (github.com/odeke-em/drive/cmd/drive)"
 echo ----------
-echo "3) If the Google_Drive folder is not found in the current directory, it will be made in the current working directory. This will contain the files uploaded to Google Drive. "
+echo "3) If 7ZIP is not installed, it will be installed via apt-get."
 echo ----------
 echo "4) Google drive will be initialized and you will need to sign in."
 echo ----------
-echo "5) After this the other commands will be ready to automate archiving, as long as the scripts are located in the same directory as Google_Drive folder."
+echo "5) After this the other commands will be ready to automate archiving, and symlinks to them are placed in ~/bin/ for easy access."
 echo ----------
 read -p "Are you ready to continue? [Y/n]:" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -22,12 +22,12 @@ then
   exit 1
 fi
 
-if ! [ -d "$SCRIPTDIR/go" ] ; then
+if ! [ -d "$SCRIPTDIR/go" ] | [ $(which go) ] ; then
 	echo "Downloading GO..."
 	sudo curl https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz --output go$VERSION.$OS-$ARCH.tar.gz
 	sudo tar -C "$SCRIPTDIR" -xzf go$VERSION.$OS-$ARCH.tar.gz
+  export GOROOT=$SCRIPTDIR/go
 fi
-export GOROOT=$SCRIPTDIR/go
 
 if ! [ -d "$SCRIPTDIR/gocode" ] ; then
   mkdir "$SCRIPTDIR/gocode"
@@ -36,8 +36,13 @@ fi
 export GOPATH=$SCRIPTDIR/gocode
 
 if ! [ -f "$GOPATH/bin/drive" ] ; then
-	echo "Installing Drive Client, please wait..."
+	echo "Installing Drive Client, which may take a long time; please wait..."
 	$GOROOT/bin/go get -u github.com/odeke-em/drive/cmd/drive
+fi
+
+if ! [ $(which 7z) ] ; then # if 7zip is not installed, install it.
+  echo "Installing 7zip using apt-get..."
+  sudo apt-get install p7zip
 fi
 
 if ! [ -d "Google_Drive" ] ; then
