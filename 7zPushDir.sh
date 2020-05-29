a0a2ee4036996cd459ec11fd198a820e963dbce3
Nicholas Hedges <nshedges@aggiemail.usu.edu>
2020-05-29 14:59:38 -0600

    Repair bugs in 7zPushDir script

--- 7zPushDir.sh
+++ 7zPushDir.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ $# -lt 2 ] ; then
-  echo "Usage: 7zPushDir [archive name.7z] [DirectoryName/]
+  echo "Usage: 7zPushDir [archive name.7z] [DirectoryName/]"
+  exit
+fi
 DIR=$(realpath $0 | grep -o -i "^.*\/")
 #a useful one-liner which will give you the full directory name of the script no matter where it is being called from.
 "$DIR/7zPushFiles.sh" "$@"*
