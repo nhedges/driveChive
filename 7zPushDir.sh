1168acf1705c598c6306d7b542d5a56d5ca7396e
Nicholas Hedges <nshedges@aggiemail.usu.edu>
2020-05-28 23:54:48 -0600

    Change go and drive install to stay inside repo folder, and auto add symlinks to bin.

--- 7zPushDir.sh
+++ 7zPushDir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #a useful one-liner which will give you the full directory name of the script no matter where it is being called from.
-"$DIR/7zPushFiles.sh" "$@"/*
+"$DIR/7zPushFiles.sh" "$@"*
 
--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 OS=linux
 ARCH=amd64
 VERSION=1.13.4
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Welcome to the driveChive installer. This script was made to set up convenient and fast archiving on linux. It has a few steps."
 echo ----------
 echo "1) If GO has not been installed in /usr/local, it will be downloaded from google and extracted/installed in /usr/local."
@@ -21,21 +22,29 @@ then
   exit 1
 fi
 
-if ! [ -d "/usr/local/go" ] ; then
+if ! [ -d "$SCRIPTDIR/go" ] ; then
 	echo "Downloading GO..."
 	sudo curl https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz --output go$VERSION.$OS-$ARCH.tar.gz
-	sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+	sudo tar -C "$SCRIPTDIR" -xzf go$VERSION.$OS-$ARCH.tar.gz
 fi
+export GOROOT=$SCRIPTDIR/go
+export GOPATH=$SCRIPTDIR/gocode
 
 if ! [ -f "$HOME/go/bin/drive" ] ; then
 	echo "Installing Drive Client, please wait..."
-	/usr/local/go/bin/go get -u github.com/odeke-em/drive/cmd/drive
+	$GOROOT/bin/go get -u github.com/odeke-em/drive/cmd/drive
 fi
 
 if ! [ -d "Google_Drive" ] ; then
     mkdir Google_Drive
     chown -R `stat -c '%U' $HOME` Google_Drive
 fi
+if ! [ -d "$HOME/bin" ] ; then
+  mkdir "$HOME/bin"
+fi
+ln -s $SCRIPTDIR/7zPushFiles.sh $HOME/bin/7zPushFiles
+ln -s $SCRIPTDIR/7zPushDir.sh $HOME/bin/7zPushDir
+
 echo "Initializing Google Drive directory. Ready to Log in."
-$HOME/go/bin/drive init ./Google_Drive
+$GOPATH/bin/drive init $SCRIPTDIR/Google_Drive
 echo "Google drive auto upload is ready. use other script(s) to automatically archive things onto Google Drive."
